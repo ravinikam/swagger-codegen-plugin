0ca80f6e41c47e46fb8d013382dc98bb35864659
ravinikam <ravinikam@gmail.com>
2019-03-21 22:29:05 -0500

    add sample test

---
+++ src/test/java/com/feg29/example/swagger/plugin/SwaggerCodegenPluginTest.java
@@ -0,0 +1,37 @@
+package com.feg29.example.swagger.plugin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.Set;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.jupiter.api.Test;
+
+import com.feb29.example.swagger.plugin.task.SwaggerGenerateTask;
+
+class SwaggerCodegenPluginTest {
+
+	File input = new File("src/test/resources/petstore.yaml");
+	@Test
+	void test() {
+		Project project = ProjectBuilder.builder().build();
+		//project.getPluginManager().apply("swagger-codegen");
+		project.getPluginManager().apply(SwaggerCodegenPlugin.class);
+		
+		Task myTask = project.getTasks().getByName("swaggerGenerate");
+		assertTrue(myTask instanceof SwaggerGenerateTask);		
+		
+		SwaggerGenerateTask generateTask = (SwaggerGenerateTask) myTask;
+		generateTask.setVerbose(true);
+		generateTask.setValidateSpec(false);
+		generateTask.setInputSpec(input);
+		generateTask.setGeneratorName("spring");
+		generateTask.setOutputDir(new File("build"));
+		generateTask.generate();	
+	}
+
+}
